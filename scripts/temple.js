d5997fa18db081b67b75971c772938533af4d3fe
JacobK44 <jacobkwartengamoah@gmail.com>
2025-01-29 12:36:25 +0000

    add code

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,14 +1,30 @@
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu");
+    const menuButton = document.querySelector("#menu");
     const navList = document.querySelector(".navigation");
 
     menuButton.addEventListener("click", function () {
       navList.classList.toggle("show");
       menuButton.classList.toggle("show");
     });
-  });
 
 
 
-// Footer 
+// Footer year and last modified
+
+const footerElement = document.getElementById("year");
+
+const recentYear = new Date().getFullYear();
+
+footerElement.innerHTML = `<span>© ${recentYear},❤  Jacob Amoah  ❤  Greater Accra.</span>`
+
+
+
+
+  // Function to populate the last modified date
+  function setLastModified() {
+    const modifiedDate = document.lastModified;
+    document.querySelector("#lastModified").textContent = `Last Modified: ${modifiedDate}`;
+  }
+
+  
+setLastModified();
