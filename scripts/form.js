e26a7f589166f57ce3594dac31678bbcde92e155
JacobK44 <jacobkwartengamoah@gmail.com>
2025-02-19 18:44:35 +0000

    add code

--- scripts/form.js
+++ scripts/form.js
@@ -56,3 +56,10 @@ document.querySelector("#lastModified").textContent = `Last Modified: ${modified
 
 
 setLastModified();
+
+
+
+let scores = [56, 78, 95, 77, 65, 86];
+let target = scores.indexOf(78);
+
+console.log(target);
